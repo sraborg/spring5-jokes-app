3951dd11a44cb2702115924cfe8dd13ccc1f1d20
Sean Raborg <sean.raborg@gmail.com>
2021-11-14 23:04:24 -0700

    Added Joke Controller. Closes #4.

---
+++ src/main/java/com/sraborg/spring5jokesapp/controllers/JokeController.java
@@ -0,0 +1,26 @@
+package com.sraborg.spring5jokesapp.controllers;
+
+import com.sraborg.spring5jokesapp.services.JokesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokesService jokesService;
+
+    @Autowired
+    public JokeController(JokesService jokesService) {
+        this.jokesService = jokesService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+
+        model.addAttribute("joke", jokesService.getJoke());
+
+        return "index";
+    }
+}
